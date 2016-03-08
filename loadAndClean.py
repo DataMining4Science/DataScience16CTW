b4253eac4187015f11d8686baa220780f0324f08
Mac-I Crowell <macicrowell@gmail.com>
2016-03-08 13:01:20 -0500

    added function to do cross validation

--- loadAndClean.py
+++ loadAndClean.py
@@ -1,8 +1,20 @@
 import pandas as pd
+import numpy as np
 
 def loadData():
 	data = pd.read_csv('IPPS_2013.csv')
 	return data
+
+def get_series_ids(x):
+    '''Function returns a pandas series consisting of ids, 
+       corresponding to objects in input pandas series x
+       Example: 
+       get_series_ids(pd.Series(['a','a','b','b','c'])) 
+       returns Series([0,0,1,1,2], dtype=int)'''
+
+    values = np.unique(x)
+    values2nums = dict(zip(values,range(len(values))))
+    return x.replace(values2nums)
 
 def str_to_num(s):
     """
@@ -16,6 +28,7 @@ def cleanData(data):
 	data['Average Total Payments Num'] = data['Average Total Payments'].apply(str_to_num)
 	data['Average Medicare Payments Num'] = data['Average Medicare Payments'].apply(str_to_num)
 	data['DRG Code'] = data['DRG Definition'].apply(lambda x: int(x.split(" - ")[0]))
+	data['Provider HRR Num'] = get_series_ids(data['Provider HRR'])
 	geo_data = pd.read_csv('provider_geocodes_google.csv')
 	data = data.merge(geo_data, how='left')
 
